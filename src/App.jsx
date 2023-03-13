c4836c20828254ac32a6ea3b08a11cd9ae439ea1
Miles <miles.collins@ymail.com>
2023-03-12 22:04:33 -0600

    Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar.jsx";
-
+// import "sweetalert2/themes/dark";
 export function App() {
   return (
     <div className="App" id="app">
       <header>
         <Navbar />
       </header>
-
+      z
       <main>
         <Outlet />
       </main>
