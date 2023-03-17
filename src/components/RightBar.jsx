5e8c0eef0d8301ca7b08c30b30738cfa3d74e9d4
Miles <miles.collins@ymail.com>
2023-03-17 14:28:34 -0600

    sync

---
+++ src/components/RightBar.jsx
@@ -0,0 +1,18 @@
+!import React from 'react';
+
+export default function componentName() {
+
+  const contacts = 
+
+  return (
+
+    <div className="componentName">
+      <div className="row">
+        <div className="col-12">
+          <h1>Contacts</h1>
+        </div>
+      </div>
+    </div>
+  )
+
+}
