fa3a24e534cadc475c750d1a2f9300be9fc4f23a
Miles <miles.collins@ymail.com>
2023-03-23 17:03:41 -0600

    Likes

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,9 +1,16 @@
 export class Account {
   constructor(data) {
-    this.id = data.id
-    this.email = data.email
-    this.name = data.name
-    this.picture = data.picture
+    this.id = data.id;
+    this.name = data.name;
+    this.email = data.email;
+    this.picture = data.picture;
+    this.coverImg = data.coverImg;
+    this.class = data.class;
+    this.github = data.github;
+    this.linkedin = data.linkedin;
+    this.resume = data.resume;
+    this.graduated = data.graduated;
+    this.bio = data.bio;
     // TODO add additional properties if needed
   }
 }
